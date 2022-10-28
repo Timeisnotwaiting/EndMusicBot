b28f642b2ab9b89b6897eb850f4f21aef5d250af
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-29 01:22:44 +0530

    Update Youtube.py

--- Functions/Youtube.py
+++ Functions/Youtube.py
@@ -1,1 +1,19 @@
+from pytube import YouTube
 
+def download_video(url):
+    x = YouTube(url)
+    try:
+        y = x.streams.get_highest_resolution()
+        z = y.download()
+        return z
+    except:
+        return None
+
+def download_audio(url):
+    x = YouTube(url)
+    try:
+        y = x.streams.get_audio_only()
+        z = y.download()
+        return z
+    except:
+        return None
