35aeb1757814a9143d11d982b5d9ac890d17a948
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-28 10:46:57 +0530

    Update keshav.py

--- keshav.py
+++ keshav.py
@@ -6,7 +6,7 @@ alpha = Client(":BOT:", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 yashu = Client(":ASST:", api_id=API_ID, api_hash=API_HASH, string_session=STRING_SESSION)
 
-x = PyTgCalls(yashu)
+asst = PyTgCalls(yashu)
 
 
 
