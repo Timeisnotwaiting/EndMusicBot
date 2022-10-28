2bdb361ec7c3fcadc4bc7497bd3a4a148540f568
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-28 10:57:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+API_ID = int(os.environ["API_ID"])
+
+API_HASH = os.environ["API_HASH"]
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+STRING_SESSION = os.environ["STRING_SESSION"]
+
+MONGO_DB_URL = os.environ["MONGO_DB_URL"]
+
