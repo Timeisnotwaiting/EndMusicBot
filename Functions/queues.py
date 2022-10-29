ec31cbe0fd11af698f59a4b881d6adcc3b59a892
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-29 10:37:18 +0530

    Update queues.py

--- Functions/queues.py
+++ Functions/queues.py
@@ -1,1 +1,14 @@
 queue = {}
+
+
+def queue_audio(chat_id, details):
+    in_queue = len(queue[chat_id])
+    next = in_queue + 1
+    queue[chat_id][in_queue] = details
+
+def queue_video(chat_id, details):
+    in_queue = len(queue[chat_id])
+    next = in_queue + 1
+    queue[chat_id][in_queue] = details
+
+    
