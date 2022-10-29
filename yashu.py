21110a70992895da023e32574e5213094eb13ed9
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-29 14:29:27 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,8 +1,10 @@
 from keshav import alpha, asst
 from pyrogram import filters, idle
+from EndMusic.play import play
 
-
-
+@alpha.on_message(filters.command(["play", "vplay"]))
+async def play_plug(_, m):
+    await play(_, m)
 
 alpha.start()
 asst.start()
